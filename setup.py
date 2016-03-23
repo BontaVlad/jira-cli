88c0944b25fec90b52cd54170844afb713d702aa
Bonta Vlad <bonta.vlad@gmail.com>
2016-03-23 16:50:24 +0200

    Initial commit as a package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='jira-cli',
+    version='0.1',
+    py_modules=['jira_cli'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        jira-cli=jira_cli:cli
+    ''',
+)
