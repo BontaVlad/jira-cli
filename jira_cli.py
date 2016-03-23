6579e6a02ab4c578b42650213d1d75543b1adf85
Bonta Vlad <bonta.vlad@gmail.com>
2016-03-23 17:08:54 +0200

    separated commands from the actual cli

--- jira_cli.py
+++ jira_cli.py
@@ -1,6 +1,29 @@
 import click
 
-@click.command()
-def cli():
+import commands
+
+
+class Config(object):
+
+    def __init__(self):
+        self.verbose = False
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+@click.group()
+@click.option('--verbose', is_flag=True)
+@pass_config
+def cli(config, verbose):
     """Hello world script"""
-    click.echo('Hello world!')
+    config.verbose = verbose
+
+
+@cli.command()
+@click.option('--string', default='World',
+              help='This is the thing is greeted')
+@click.option('--repeat', default=1,
+              help='How many times should be repeated')
+@pass_config
+def say(config, string, repeat):
+    commands.say(config, string, repeat)
