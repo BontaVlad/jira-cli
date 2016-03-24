9c715b9ba15c59eed29e2aa585a6705edd753e3c
Bonta Vlad <bonta.vlad@gmail.com>
2016-03-24 17:00:17 +0200

    Stuf client for now. Problems with jira authentification.

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+import ssl
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -7,17 +8,19 @@ class Client(object):
     # TODO: make it configurable
     base_url = 'https://jira.atlassian.com/rest/api/2'
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    # def __init__(self, username, password):
+    #     self.username = username
+    #     self.password = password
+    def __init__(self):
+        pass
 
-    def _get(self, url, *args, **kwargs):
-        if not all([self.username, self.password]):
-            raise ValueError("Login credentials not provided")
-        kwargs['auth'] = HTTPBasicAuth(self.username, self.password)
-        kwargs['params'] = kwargs.get('params', {})
-        return requests.get(url, *args, **kwargs)
-
-    def get_issue(self, issue_id):
-        url = self.base_url + '/issue/{}'.format(issue_id)
-        return self._get(url)
+    # def _get(self, url, *args, **kwargs):
+    #     if not all([self.username, self.password]):
+    #         raise ValueError("Login credentials not provided")
+    #     kwargs['auth'] = HTTPBasicAuth(self.username, self.password)
+    #     kwargs['params'] = kwargs.get('params', {})
+    #     return requests.get(url, *args, **kwargs)
+    #
+    # def get_issue(self, issue):
+    #     url = self.base_url + '/issue/{}'.format(issue)
+    #     return self._get(url)
